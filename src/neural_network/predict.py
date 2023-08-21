adf82d4cb58fc8e2f454e5ef7705d739eb2ced13
siavash-saki <siavash.saki@engineer.com>
2023-08-21 13:05:24 +0200

    STYL: Reformat Code

--- src/neural_network/predict.py
+++ src/neural_network/predict.py
@@ -2,19 +2,24 @@ from model import *
 from train import *
 import tensorflow as tf
 
+
 def load_model(path="path/to/load/from"):
     return tf.keras.models.load_model(path)
+
 
 def load_new_data():
     # Load new data for prediction
     pass
 
+
 def preprocess_new_data(data):
     # Preprocess the new data
     pass
 
+
 def make_prediction(model, data):
     return model.predict(data)
+
 
 if __name__ == "__main__":
     model = load_model()
